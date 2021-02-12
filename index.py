3892d0ccfcd32331011102566ffa032f9c05e985
Kinetix <x86app@126.com>
2021-02-12 22:11:09 +0800

    完善配置文件载入代码
    1. 增加开发配置文件，方便控制（后期打算继续增加）
    2. 打开配置文件时显式声明只读模式
    3. 增加关闭文件的代码

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import urllib3, sys, toml
+from development import devVariables
 http = urllib3.PoolManager()
 
 paths = {
@@ -9,21 +10,26 @@ paths = {
   'stat': 'https://api.bilibili.com/x/relation/stat',
   'upstat': 'https://api.bilibili.com/x/space/upstat'
 }
+dev = devVariables()
 
-print('BiliRank - 哔哩哔哩数据自动化程序\nhttps://github.com/Kinetix-Lee/bilirank\n')
+print('\nBiliRank - 哔哩哔哩数据自动化程序')
+print('https://github.com/Kinetix-Lee/bilirank\n')
 
 # 读取配置文件
-print('正在读取配置文件')
+print('正在载入配置文件')
 try:
-  bilirank_toml = open(paths['config'])
-  config = toml.load(bilirank_toml)
-  print(config)
+  f_bilirank_toml = open(paths['config'], 'r')
+  config = toml.load(f_bilirank_toml)
+  print('配置文件载入成功')
+  print(config) if dev['printConfig'] else True;
 except OSError as err: 
-  print('配置文件读取错误：\n{0}'.format(err))
+  print('配置文件读取错误\n{0}'.format(err))
 except KeyboardInterrupt as err:
-  print('程序退出\n')
+  print('程序退出')
 except:
-  print('未知错误：\n', sys.exc_info()[0])
+  print('未知错误', sys.exc_info()[0])
+finally:
+  f_bilirank_toml.close()
 
 # req_stat = http.request('GET', api['stat'], 
 #                   fields={
